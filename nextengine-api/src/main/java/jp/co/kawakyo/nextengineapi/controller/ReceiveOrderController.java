7b19b4e4e2e57b1ebce33772fa4b349388550e2d
satokent <satokent@kawakyoB5.kawakyo.local>
2021-10-19 22:32:15 +0900

    20211019 受注登録画面のフロント作成（商品情報未達）

--- nextengine-api/src/main/java/jp/co/kawakyo/nextengineapi/controller/ReceiveOrderController.java
+++ nextengine-api/src/main/java/jp/co/kawakyo/nextengineapi/controller/ReceiveOrderController.java
@@ -1,5 +1,21 @@
 package jp.co.kawakyo.nextengineapi.controller;
 
-public class ReceiveOrderController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.kawakyo.nextengineapi.base.BaseController;
+
+@Controller
+public class ReceiveOrderController extends BaseController {
+
+	@RequestMapping(value="/registOrder", method = RequestMethod.GET)
+	private String showRegistOrderView(HttpServletRequest _request, HttpServletResponse _response, Model model) {
+		return "registOrder";
+	}
+	
 }
